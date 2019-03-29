b0bdbd35153f2da5f459cd86be2cce4a37c1c467
bianyuqiu <bianyuqiu@tuniu.org>
2019-03-29 18:03:27 +0800

    报警通知改成集合形式

--- umsj-common/src/main/java/com/tuniu/bi/umsj/service/WebHookServiceImpl.java
+++ umsj-common/src/main/java/com/tuniu/bi/umsj/service/WebHookServiceImpl.java
@@ -2,6 +2,7 @@ package com.tuniu.bi.umsj.service;
 
 import com.alibaba.fastjson.JSONObject;
 import com.tuniu.bi.umsj.exception.AbstractException;
+import com.tuniu.bi.umsj.vo.AlertItem;
 import com.tuniu.bi.umsj.vo.AlertManagerRequestVO;
 import com.tuniu.bi.umsj.vo.MessageRequestVO;
 import io.micrometer.core.instrument.util.StringUtils;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.*;
 
 /**
@@ -37,11 +39,9 @@ public class WebHookServiceImpl implements WebHookService {
     @Override
     public void sendMessage(AlertManagerRequestVO requestVO) throws AbstractException {
         List<String> nameList = new ArrayList<>(biPlatform);
-        MessageRequestVO messageRequestVO = new MessageRequestVO();
-        messageRequestVO.setTitle(requestVO.getCommonAnnotations().getSummary());
-        messageRequestVO.setContent(requestVO.getCommonAnnotations().getDescription());
-        messageRequestVO.setNames(nameList);
-        logger.info("RequestVO: {}", JSONObject.toJSONString(requestVO));
+        List<AlertItem> alertItems = requestVO.getAlerts();
+
+        //判断alertName包含发送消息类型
         Set<String> sendTypes = new HashSet<>();
         String alertName = requestVO.getGroupLabels().getAlertname();
         if (StringUtils.isNotBlank(alertName)) {
@@ -57,29 +57,37 @@ public class WebHookServiceImpl implements WebHookService {
                 sendTypes.add("2");
             }
         }
-        for (String str:sendTypes) {
-            switch (str) {
-                case "1":
-                    messageRequestVO.setType(1);
-                    dingTalkService.sendMessage(messageRequestVO);
-                    logger.info("发送钉钉成功");
-                    System.out.println("发送钉钉成功");
-                    break;
-                case "2":
-                    messageRequestVO.setType(2);
-                    emailService.sendMessage(messageRequestVO);
-                    logger.info("发送邮件成功");
-                    System.out.println("发送邮件成功");
-                    break;
-                case "3":
-                    messageRequestVO.setType(3);
-                    smsService.sendMessage(messageRequestVO);
-                    logger.info("发送短信成功");
-                    System.out.println("发送短信成功");
-                    break;
-                default:
-                    break;
+        for (AlertItem item : alertItems) {
+            MessageRequestVO messageRequestVO = new MessageRequestVO();
+            messageRequestVO.setTitle(item.getAnnotations().getSummary());
+            messageRequestVO.setContent(item.getAnnotations().getDescription());
+            messageRequestVO.setNames(nameList);
+            logger.info("发送消息体: RequestVO: {}", JSONObject.toJSONString(messageRequestVO));
+            for (String str : sendTypes) {
+                switch (str) {
+                    case "1":
+                        messageRequestVO.setType(1);
+                        dingTalkService.sendMessage(messageRequestVO);
+                        logger.info("发送钉钉成功");
+                        System.out.println("发送钉钉成功");
+                        break;
+                    case "2":
+                        messageRequestVO.setType(2);
+                        emailService.sendMessage(messageRequestVO);
+                        logger.info("发送邮件成功");
+                        System.out.println("发送邮件成功");
+                        break;
+                    case "3":
+                        messageRequestVO.setType(3);
+                        smsService.sendMessage(messageRequestVO);
+                        logger.info("发送短信成功");
+                        System.out.println("发送短信成功");
+                        break;
+                    default:
+                        break;
+                }
             }
+
         }
     }
 }
