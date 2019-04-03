a850a0a2b9903e60e52be6738b3e0f2ae288ebd3
张威21 <zhangwei21@tuniu.com>
2019-04-03 17:14:57 +0800

    增加同步接口，查询用户的salerId和phone

--- umsj-common/src/main/java/com/tuniu/bi/umsj/controller/api/FixController.java
+++ umsj-common/src/main/java/com/tuniu/bi/umsj/controller/api/FixController.java
@@ -1,10 +1,18 @@
 package com.tuniu.bi.umsj.controller.api;
 
+import com.tuniu.bi.umsj.exception.AbstractException;
+import com.tuniu.bi.umsj.exception.InvalidParamException;
 import com.tuniu.bi.umsj.service.UserService;
+import com.tuniu.bi.umsj.utils.ResponseUtils;
+import com.tuniu.bi.umsj.vo.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author zhangwei21
@@ -21,9 +29,22 @@ public class FixController {
      *
      * @return
      */
-    @RequestMapping(value = "syncOaUser", method = RequestMethod.POST)
-    public String syncOaUser() {
+    @RequestMapping(value = "/syncOaUser", method = RequestMethod.POST)
+    public Response syncOaUser() {
         userService.syncOaUserInfo();
-        return "同步成功";
+        return ResponseUtils.success();
+    }
+
+    /**
+     * 补充用户信息
+     * @return
+     */
+    @RequestMapping(value = "/supplyUserInfo", method = RequestMethod.POST)
+    public Response supplyUserInfo(@RequestBody List<String> usernames) throws AbstractException {
+        if (CollectionUtils.isEmpty(usernames)) {
+            throw new InvalidParamException("参数错误");
+        }
+        userService.supplyUserInfo(usernames);
+        return ResponseUtils.success();
     }
 }
