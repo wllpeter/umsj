a9cebd6e6274f3ad4a21463af0b08c18e237bcc2
张威21 <zhangwei21@tuniu.com>
2019-04-10 17:57:59 +0800

    增加redis锁

--- umsj-common/src/main/java/com/tuniu/bi/umsj/aspect/RequestLimitAspect.java
+++ umsj-common/src/main/java/com/tuniu/bi/umsj/aspect/RequestLimitAspect.java
@@ -2,6 +2,7 @@ package com.tuniu.bi.umsj.aspect;
 
 import com.google.common.base.Joiner;
 import com.tuniu.bi.umsj.annotation.RequestLimit;
+import com.tuniu.bi.umsj.exception.CommonException;
 import com.tuniu.bi.umsj.exception.RequestLimitException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -16,6 +17,8 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zhangwei21
@@ -24,9 +27,15 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class RequestLimitAspect {
 
-    private static final String REQUEST_REDIS_KEY_PREFIX = "req:limit:";
+    private static final String REQUEST_REDIS_KEY_PREFIX = "req:limit";
+
+    private static final String REQUEST_REDIS_KEY_LOCK = "req:limit:lock";
+
+    private static final String REQUEST_REDIS_KEY_LOCK_VALUE = "req:limit:lock:value";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RequestLimitAspect.class);
+
+    private static final long EXPIRE_LOCK = 10;
 
     @Autowired
     private RedisTemplate redisTemplate;
@@ -42,6 +51,8 @@ public class RequestLimitAspect {
         String ip = request.getRemoteAddr();
         Joiner on = Joiner.on(":");
         String key = on.join(REQUEST_REDIS_KEY_PREFIX, url, ip);
+        // 获取锁
+        String keyLock = on.join(REQUEST_REDIS_KEY_LOCK, key);
         ListOperations<String, Long> stringLongListOperations = redisTemplate.opsForList();
         Long now = System.currentTimeMillis();
         Long size = stringLongListOperations.size(key);
@@ -59,10 +70,22 @@ public class RequestLimitAspect {
                 stringLongListOperations.leftPop(key);
             }
         }
-        Long newSize = stringLongListOperations.size(key);
-        if (newSize >= count) {
-            throw new RequestLimitException(time/60000 + "分钟内访问次数超过" + count + "次");
+        try {
+            if (redisTemplate.opsForValue().setIfAbsent(keyLock, REQUEST_REDIS_KEY_LOCK_VALUE, EXPIRE_LOCK, TimeUnit.SECONDS)) {
+                Long newSize = stringLongListOperations.size(key);
+                if (newSize >= count) {
+                    throw new RequestLimitException(time/60000 + "分钟内访问次数超过" + count + "次");
+                }
+                stringLongListOperations.rightPush(key,now);
+                if (newSize == null || newSize == 0) {
+                    // key设置超时时间，避免redis空间占用太多,
+                    redisTemplate.expire(key, time * 60, TimeUnit.MILLISECONDS);
+                }
+            } else {
+                throw new CommonException("获取锁失败");
+            }
+        } finally {
+            redisTemplate.delete(keyLock);
         }
-        stringLongListOperations.rightPush(key,now);
     }
 }
