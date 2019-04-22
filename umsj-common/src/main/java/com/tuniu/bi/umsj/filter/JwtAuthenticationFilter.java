fb9745430b04dd00d0d5b9de5197258740faabd7
张威21 <zhangwei21@tuniu.com>
2019-04-22 14:20:17 +0800

    增加jwt配置

--- umsj-common/src/main/java/com/tuniu/bi/umsj/filter/JwtAuthenticationFilter.java
+++ umsj-common/src/main/java/com/tuniu/bi/umsj/filter/JwtAuthenticationFilter.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author zhangwei21
+ */
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final PathMatcher pathMatcher = new AntPathMatcher();
@@ -20,19 +23,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         try {
             if(isProtectedUrl(request)) {
                 String token = request.getHeader("token");
-                //检查jwt令牌, 如果令牌不合法或者过期, 里面会直接抛出异常, 下面的catch部分会直接返回
                 JwtUtils.validateToken(token);
             }
         } catch (Exception e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
             return;
         }
-        //如果jwt令牌通过了检测, 那么就把request传递给后面的RESTful api
         filterChain.doFilter(request, response);
     }
 
-
-    //我们只对地址 /api 开头的api检查jwt. 不然的话登录/login也需要jwt
     private boolean isProtectedUrl(HttpServletRequest request) {
         return pathMatcher.match("/api/**", request.getServletPath()) && !pathMatcher.match("/api/user/login", request.getServletPath());
     }
--- umsj-common/src/main/java/com/tuniu/bi/umsj/utils/JwtUtils.java
+++ umsj-common/src/main/java/com/tuniu/bi/umsj/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.tuniu.bi.umsj.utils;
 
+import com.tuniu.bi.umsj.constant.SecurityConfigConst;
 import com.tuniu.bi.umsj.constant.SecurityConst;
 import com.tuniu.bi.umsj.exception.CommonException;
 import io.jsonwebtoken.Jwts;
@@ -16,33 +17,34 @@ public class JwtUtils {
 
     /**
      * 生成token
+     *
      * @param username
      * @return
      */
     public static String generateToken(String username) {
-        HashMap<String, Object> map = new HashMap<>();
-        //you can put any data in the map
+        Map<String, Object> map = new HashMap<>(16);
         map.put("username", username);
         String jwt = Jwts.builder()
                 .setClaims(map)
-                .setExpiration(new Date(System.currentTimeMillis() + 1800000))
-                .signWith(SignatureAlgorithm.HS512, SecurityConst.SIGNING_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConfigConst.expire))
+                .signWith(SignatureAlgorithm.HS512, SecurityConfigConst.key)
                 .compact();
-        return "Bearer "+jwt; //jwt前面一般都会加Bearer
+        return "Bearer " + jwt;
     }
 
     /**
      * 校验token
+     *
      * @param token
      */
     public static void validateToken(String token) {
         try {
-            Map<String, Object> body = Jwts.parser()
+            Jwts.parser()
                     .setSigningKey(SecurityConst.SIGNING_KEY)
-                    .parseClaimsJws(token.replace("Bearer ",""))
+                    .parseClaimsJws(token.replace("Bearer ", ""))
                     .getBody();
-        }catch (Exception e){
-            throw new CommonException("Invalid Token. "+e.getMessage());
+        } catch (Exception e) {
+            throw new CommonException("Invalid Token. " + e.getMessage());
         }
     }
 }
