3bf94cf0662b68910cf349b11b0ad7aef1719642
张威21 <zhangwei21@tuniu.com>
2019-05-05 11:04:17 +0800

    增加clearContext

--- src/main/java/com/tuniu/bi/umsj/base/filter/JwtLoginFilter.java
+++ src/main/java/com/tuniu/bi/umsj/base/filter/JwtLoginFilter.java
@@ -1,18 +1,18 @@
 package com.tuniu.bi.umsj.base.filter;
 
 import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tuniu.bi.umsj.base.utils.JwtUtils;
 import com.tuniu.bi.umsj.base.utils.ResponseUtils;
 import com.tuniu.bi.umsj.base.vo.Response;
 import com.tuniu.bi.umsj.base.vo.User;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.util.StringUtils;
@@ -21,6 +21,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -80,14 +81,29 @@ public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
         map.put("token", token);
         Response<Map<String, String>> success = ResponseUtils.success(map);
         response.getWriter().write(JSONObject.toJSONString(success));
+        clearAuthenticationAttributes(request);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        SecurityContextHolder.clearContext();
         Response<Map<String, String>> error = ResponseUtils.custom(false, HttpStatus.UNAUTHORIZED.value(), failed.getMessage());
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setCharacterEncoding("UTF-8");
         response.setHeader("Content-type", request.getHeader("Content-Type"));
         response.getWriter().write(JSONObject.toJSONString(error));
+    }
+
+    /**
+     *
+     * @param request
+     */
+    private final void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
     }
 }
